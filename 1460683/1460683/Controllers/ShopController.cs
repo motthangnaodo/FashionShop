6ff6f4c7ac8def182ab2070cc314da7a5abcaa3c
motthangnaodo <motthangnaodo1995@gmail.com>
2017-04-23 17:14:17 +0700

    LOAD SANPHAM-DETAIL-PHANTRANG

--- 1460683/1460683/Controllers/ShopController.cs
+++ 1460683/1460683/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _1460683.Models.BUS;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,15 +11,18 @@ namespace _1460683.Controllers
     public class ShopController : Controller
     {
         // GET: Shop
-        public ActionResult Index()
+        public ActionResult Index(int page=1,int pagesize =3)
         {
-            return View();
+            var db =ShopBUS.DanhSach().ToPagedList(page,pagesize);
+            return View(db);
+
         }
 
         // GET: Shop/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(String id)
         {
-            return View();
+            var db = ShopBUS.ChiTiet(id);
+            return View(db);
         }
 
         // GET: Shop/Create
---
+++ 1460683/1460683/Models/BUS/ShopBUS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FashionShopConnection;
+
+namespace _1460683.Models.BUS
+{
+    public class ShopBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new FashionShopConnectionDB();
+            return db.Query<SanPham>("Select * from SanPham where TinhTrang = 1");
+        }
+        public static SanPham ChiTiet(String a)
+        {
+            var db = new FashionShopConnectionDB();
+            return db.SingleOrDefault<SanPham>("Select * from SanPham where MaSanPham=@0",a);
+        }
+    }
+}
